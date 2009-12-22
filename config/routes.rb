6949b132eeb16d47ee31aae55a47fc1b3c3e8a12
versus <versus@linux-85fl.site>
2009-12-22 15:09:08 +0200

    done auth, add controller task, project, comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
-
+  map.resource :account, :controller => "users"
+  map.resources :users
   map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new"
+  
   #map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
   
   # The priority is based upon order of creation: first created -> highest priority.
@@ -42,7 +45,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
 end
 
